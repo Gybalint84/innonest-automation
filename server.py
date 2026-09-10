2edf713d7c3e77c2a8c6d501f885e26b841dc500
Gybalint84 <info@sqm-hungary.hu>
2026-09-10 11:09:17 +0200

    Update server.py

--- server.py
+++ server.py
@@ -83,6 +83,8 @@ from innonest_szamlalo import register_innonest_szamlalo_routes
 register_innonest_szamlalo_routes(app)
 from szamla_ellenorzo import register_szamla_routes
 register_szamla_routes(app)
+   from szamlazz_adatkapcsolat import register_szamlazz_routes
+   register_szamlazz_routes(app)
 # ── Indítás ───────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
